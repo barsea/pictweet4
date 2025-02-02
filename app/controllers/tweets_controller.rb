49681a540afa77c99673ee35227514cfde7093dd
barsea <92622141+barsea@users.noreply.github.com>
2025-02-02 12:29:35 +0900

    Refactor: before_action を導入し、@tweet の取得処理を共通化

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
 
   def index
     @tweets = Tweet.all
@@ -21,7 +22,6 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end
 
   def update
@@ -31,12 +31,15 @@ class TweetsController < ApplicationController
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
   end
 
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 
 end
