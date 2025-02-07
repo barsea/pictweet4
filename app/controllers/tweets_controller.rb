8cba57b7097c7a5e338cf0348e6f1b2fd1c621ba
barsea <92622141+barsea@users.noreply.github.com>
2025-02-07 18:01:44 +0900

    tweetsテーブルのnameカラムが不要になったので削除

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -37,7 +37,8 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-    params.require(:tweet).permit(:name, :image, :text).merge(user_id: current_user.id)
+    # params.require(:tweet).permit(:name, :image, :text).merge(user_id: current_user.id)
+    params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
   end
 
   def set_tweet
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_04_091058) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_07_084841) do
   create_table "tweets", charset: "utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", null: false
